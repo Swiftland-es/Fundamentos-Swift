80cefa9784d3b1f2bb29ba8db0d7fcc7d3a2783d
Ignasi Perez <perezignasi@gmail.com>
2019-10-18 12:30:06 +0200

    Initial Commit

---
+++ Prototipo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo1
+//
+//  Created by ignasi - Swiftland.training on 18/10/2019.
+//  Copyright © 2019 Swiftland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
